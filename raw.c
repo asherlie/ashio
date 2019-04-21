8cc877cd6d2726226f2ae86eea1a73d9913ddcc9
asherlie <lieber.asher@gmail.com>
2019-04-21 16:50:18 -0400

    added a tab_complete() function

--- raw.c
+++ raw.c
@@ -73,3 +73,61 @@ char* getline_raw(int* bytes_read, _Bool* tab, int* ignore){
 
       return ret;
 }
+
+/*char* tab_complete(char** opts, int optlen){*/
+/* data_offset is offset into data where char* can be found
+ * this is set to 0 if data_douplep is a char*
+ */
+/* tab_complete behaves like getline(), but does not include \n char in returned string
+ */
+char* tab_complete(void* data_douplep, int data_offset, int optlen, char iter_opts, int* bytes_read){
+      _Bool tab, found_m;
+      char* ret = getline_raw(bytes_read, &tab, NULL), * tmp_ch;
+      if(tab){
+            found_m = 0;
+            _Bool select = 0;
+            int maxlen = *bytes_read, tmplen;
+            while(!select){
+                  for(int i = 0; i <= optlen; ++i){
+                        /* we treat i == optlen as input string
+                         */
+                        tmp_ch = (i != optlen) ? ((char**)(data_douplep+data_offset))[i] : ret;
+                        if(strstr(tmp_ch, ret)){
+                              found_m = 1;
+
+                              /* printing match to screen and removing chars from
+                               * old string
+                               */
+                              tmplen = strlen(tmp_ch);
+                              putchar('\r');
+                              printf("%s", tmp_ch);
+                              if(tmplen > maxlen)maxlen = tmplen;
+                              for(int i = 0; i < maxlen-tmplen; ++i)putchar(' ');
+                              putchar('\r');
+
+                              raw_mode();
+
+                              char ch;
+                              while(((ch = getc(stdin)))){
+                                    if(ch == '\r'){
+                                          *bytes_read = tmplen;
+                                          free(ret);
+                                          ret = tmp_ch;
+                                          select = 1;
+                                          break;
+                                    }
+                                    if(ch == iter_opts)break;
+                              }
+
+                              reset_term();
+
+                              if(select)break;
+                              continue;
+                        }
+                        else if(i == optlen-1 && !found_m)
+                              break;
+                  }
+            }
+      }
+      return ret;
+}
