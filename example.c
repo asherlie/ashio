76c792ee683ecb1947f585aa73aa06f2ef184a62
asherlie <lieber.asher@gmail.com>
2019-04-27 00:42:39 -0400

    added an example of tab_complete() to example.c

--- example.c
+++ example.c
@@ -8,13 +8,23 @@ gcc example.c raw.c -o ex
 
 #include "raw.h"
 
-int main(){
+int main(int a, char** b){
       int sz;
-      _Bool tab = 0;
       char* ln;
-      while((ln = getline_raw(&sz, &tab, NULL))){
+      if(a == 1){
+            _Bool tab = 0;
+            while((ln = getline_raw(&sz, &tab, NULL))){
+                  printf("\n%i %s\n", sz, ln);
+                  free(ln);
+            }
+            return 0;
+      }
+
+      puts("enter tab to trigger auto completion and 'n' to iterate through options");
+      _Bool free_s;
+      while((ln = tab_complete(b, sizeof(char*), 0, a, 'n', &sz, &free_s))){
             printf("\n%i %s\n", sz, ln);
-            free(ln);
+            if(free_s)free(ln);
       }
       return 0;
 }
