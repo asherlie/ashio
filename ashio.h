29b0cf5ef3f10594934f4c8796f67742c5a71848
asherlie <lieber.asher@gmail.com>
2019-07-11 23:32:27 -0400

    stubbed out functions for interacting with struct tabcom, added meaningful members to struct tabcom

--- ashio.h
+++ ashio.h
@@ -4,6 +4,20 @@ char* getline_raw(int* bytes_read, _Bool* tab, int* ignore);
 char* tab_complete(void* data_douplep, int data_blk_sz, int data_offset, int optlen,
                    char iter_opts, int* bytes_read, _Bool* free_s);
 
+
+/* experimental/unimplemented struct tabcom code below */
+struct tabcom init_tabcom(struct tabcom* tbc);
+void free_tabcom(struct tabcom* tbc);
+void insert_tabcom(void* data_douplep, int data_blk_sz, int data_offset, int optlen);
+
+char* tab_complete_tbc(struct tabcom tbc, char iter_opts, int* bytes_read, _Bool* free_s);
+
+struct tabcom_entry{
+      void* data_douplep;
+      int data_blk_sz, data_offset, optlen;
+};
+
 struct tabcom{
-      int placeholder;
+      struct tabcom_entry* tbce;
+      int n, sz;
 };
