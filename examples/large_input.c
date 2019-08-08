50835cdadf2113e7d39071098b8020e5d5f40058
asherlie <lieber.asher@gmail.com>
2019-08-08 01:25:51 -0400

    added example with very large input

---
+++ examples/large_input.c
@@ -0,0 +1,24 @@
+#include "ashio.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(){
+      char** cpp = malloc(sizeof(char*)*1e6);
+      for(int i = 0; i < 1e6; ++i){
+            cpp[i] = calloc(sizeof(char)*8, 1);
+            sprintf(cpp[i], "%i", i);
+      }
+
+      struct tabcom tbc;
+      init_tabcom(&tbc);
+      insert_tabcom(&tbc, cpp, sizeof(char*), 0, 1e6);
+
+      int sz;
+      _Bool fr;
+      char* ret;
+      while((ret = tab_complete(&tbc, "np", &sz, &fr)))printf("%i: \"%s\"\n", sz, ret);
+
+      free_tabcom(&tbc);
+      for(int i = 0; i < 1e6; ++i)free(cpp[i]);
+      free(cpp);
+}
