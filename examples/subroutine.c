6fee06d65453903e77951192465fd2cc92429820
asherlie <lieber.asher@gmail.com>
2019-08-13 22:02:35 -0400

    simplified subroutine example

--- examples/subroutine.c
+++ examples/subroutine.c
@@ -10,47 +10,26 @@ gcc subroutine.c ashio.c -o sub
 
 void* routine(void* arg){
       struct gr_subroutine_arg* gsa = (struct gr_subroutine_arg*)arg;
-      (void)gsa;
-      /*puts("CLLED");*/
-      printf("read a char: %c\n", *gsa->char_recvd);
+      printf("\r\nread a char: '%c', str is now: \"%s\"\n", *gsa->char_recvd, *gsa->str_recvd);
       return NULL;
 }
 
-int main(int a, char** b){
+int main(){
       int sz;
       char* ln;
-      if(a == 1){
             
-            _Bool tab = 0;
+      _Bool tab = 0;
 
-            struct gr_subroutine_arg gsa;
-            init_gsa(&gsa);
-            gsa.pthread_arg = &gsa;
+      struct gr_subroutine_arg gsa;
+      init_gsa(&gsa);
+      gsa.pthread_arg = &gsa;
 
-            while((ln = getline_raw_sub(&sz, &tab, NULL, routine, &gsa))){
-                  printf("\n%i %s\n", sz, ln);
-                  free(ln);
-            }
-
-            free_gsa(&gsa);
-
-            return 0;
-      }
-
-      struct tabcom tbc;
-      init_tabcom(&tbc);
-      insert_tabcom(&tbc, b, sizeof(char*), 0, a);
-
-      puts("enter tab to trigger auto completion and 'n' to iterate through options");
-
-      _Bool free_s;
-      char iter[2] = {'n', 'p'};
-      while((ln = tab_complete(&tbc, iter, &sz, &free_s))){
+      while((ln = getline_raw_sub(&sz, &tab, NULL, routine, &gsa))){
             printf("\n%i %s\n", sz, ln);
-            if(free_s)free(ln);
+            free(ln);
       }
 
-      free_tabcom(&tbc);
+      free_gsa(&gsa);
 
       return 0;
 }
